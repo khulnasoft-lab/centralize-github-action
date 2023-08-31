08316c275806709801f25c6930968cef78fde674
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-01 04:39:40 +0600

    Create get_latest_release.py

---
+++ create-mergeback-branch-from-release/get_latest_release.py
@@ -0,0 +1,18 @@
+max_version = (0, 0, 0, 0)
+with open("branches") as list_of_branches:
+    for branch in list_of_branches:
+        branch = branch.strip()
+        words = branch.split('/')
+        version = (words[-1:])
+        semantic = ''.join(version)
+        semantic = semantic.split('.')
+        semantic = list(map(int, semantic))
+        semantic_tuple = tuple(semantic)
+        #print(semantic_tuple)
+        if semantic_tuple > max_version:
+            if semantic_tuple[0] != 99:
+                max_version = semantic_tuple
+                branch_name = branch
+
+#print(max_version)
+print(branch_name)
